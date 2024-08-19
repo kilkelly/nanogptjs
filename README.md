880c8d5f5d38bfa6e5acd6b6e15efd5f0b7c551a
kilkelly <frank@monivea.com>
2024-08-19 14:11:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,331 @@
+# NanoGPTJS
+
+Node.js library to Interact with [NanoGPT](https://nano-gpt.com/)'s API. The NanoGPT service enables pay-per-prompt interaction with chat and image generation models. You will need a prefilled NanoGPT wallet and [API key](https://nano-gpt.com/settings) to use this library effectively.
+
+## Installation
+```sh
+npm install --save nanogptjs
+```
+
+## Sample Usage
+
+```js
+const nanogptjs = require('nanogptjs')
+const { YOUR_NANO_GPT_API_KEY } = require('./secrets.js')
+
+;(async function () {
+
+   const nanogpt = nanogptjs({
+    apiKey: YOUR_NANO_GPT_API_KEY,
+    defaultModel: 'chatgpt-4o-latest'
+  })
+
+  let { reply } = await nanogpt.chat('Tell me something about Nano digital currency?')  
+
+  console.log(reply) // Nano is a feeless and instant cryptocurrency.
+}
+
+```
+
+## Setup
+
+Importing `nanogptjs`
+```js
+const nanogptjs = require('nanogptjs')
+```
+
+Create a `nanogptjs` instance with your NanoGPT API key (required)
+```js
+const nanogpt = nanogptjs({
+  apiKey: YOUR_NANO_GPT_API_KEY
+})
+```
+
+Alternatively supply a `defaultModel` to be used in further function calls
+```js
+const nanogpt = nanogptjs({
+  apiKey: YOUR_NANO_GPT_API_KEY,
+  defaultModel: 'chatgpt-4o-latest'
+})
+```
+
+## API
+
+### chat(String prompt)
+
+Sends a chat prompt. Requires `defaultModel` field be set when creating your `nanogptjs` instance.
+
+```js
+let reply = await nanogpt.chat('Tell me something about Nano digital currency?')
+```
+
+#### Arguments
+
+- **prompt** (String, required) - This is the prompt that will be sent to the text model.
+
+Returns a Promise that resolves to an Object with the following format:
+```js
+{
+  reply: "Nano is a feeless and instant cryptocurrency.", // the reply to your chat prompt
+  metadata: {
+    nanoCost: 0.001089, // the Nano cost of this interaction
+    inputTokens: 24, // the number of tokens your input created
+    outputTokens: 21 // the number of tokens your output created
+  }
+}
+```
+
+### chat(Object config)
+
+Sends a chat prompt. Gives you more control by allowing you to specify model and chat context (i.e. history).
+
+```js
+let reply = await nanogpt.chat({
+  prompt: 'What is my favorite color?', // chat prompt
+  model: 'chatgpt-4o-latest', // model to use
+  context: [
+    nanogpt.contextUser('Blue is my favorite color'),
+    nanogpt.contextAI('Very interesting! I will remember that.'),
+  ] // context for your chat prompt
+})
+```
+
+#### Arguments
+
+- **config** (Object, required)) - This is a configuration Object containing the following fields: 
+  - **prompt** (String, required) - This is the prompt that will be sent to the text model.
+  - **model** (String, optional) - The text model to use. This field is optional if the `defaultModel` field is set when creating your `nanogptjs` instance.
+  - **context** (Array, optional) - Provide context (chat history) to your chat prompt. Send context in an alternating user-to-AI sequence. Use the utility functions `contextUser` and `contextAI` to specify who is speaking.
+
+Returns a Promise that resolves to an Object with the following format:
+```js
+{
+  reply: "Based on prior informations, I think blue is your favorite color!", // the reply to your chat prompt
+  metadata: {
+    nanoCost: 0.001089, // the Nano cost of this interaction
+    inputTokens: 24, // the number of tokens your input created
+    outputTokens: 21 // the number of tokens your output created
+  }
+}
+```
+
+### image(Object config)
+
+Generates a single image.
+
+```js
+let image = await nanogpt.image({
+  prompt: 'a beautiful landscape, bright sunshine, green fields',
+  model: 'dreamshaper_8_93211.safetensors',
+  width: 1024,
+  height: 1024
+})
+```
+
+#### Arguments
+
+- **config** (Object, required) - This is a configuration Object containing the following fields: 
+  - **prompt** (String, required) - This is the prompt for what you want to see in your generated image.
+  - **width** (Integer, optional) - The width of the image you want to generate.
+  - **height** (Integer, optional) - The height of the image you want to generate.
+  - **negativePrompt** (String, optional) - This is the prompt for what you **don't** want to see in your generated image.
+  - **steps** (Integer, optional) - Number of generation steps. Higher is better but more expensive. Aim for 25-30.
+  - **sampler** (String, optional) - Sampler that will be used to denoise your image during generation. For advanced users.
+  - **scale** (Number, optional) - Determines how much the image generation process follows the text prompt. For advanced users.
+
+Returns a Promise that resolves to an Object with the following format:
+```js
+{
+  base64: " /9j/4AAQSkZJRgABAQAAAQABAAD...", // base64 encoded version of your generated image
+  metadata: {
+    created: 1723814389930, // timestamp of this image generation
+    nanoCost: 0.0021, // the Nano cost of this interaction
+    remainingBalance: 0.10210999999999999 // your remaining NanoGPT balance
+  }
+}
+```
+
+### imageBatch(Object config)
+
+Generates a specific number of  images.
+
+```js
+let imageBatch = await nanogpt.imageBatch({
+  batchSize: 2
+  prompt: 'a beautiful landscape with a large cat',
+  model: 'dreamshaper_8_93211.safetensors',
+  width: 1024,
+  height: 1024
+})
+```
+
+#### Arguments
+
+- **config** (Object, required) - This is a configuration Object containing the following fields: 
+  - **batchSize** (Number, required) - Specify how many images to generate. The more images, the higher the Nano cost.
+  - **prompt** (String, required) - This is the prompt for what you want to see in your generated image.
+  - **width** (Integer, optional) - The width of the image you want to generate.
+  - **height** (Integer, optional) - The height of the image you want to generate.
+  - **negativePrompt** (String, optional) - This is the prompt for what you **don't** want to see in your generated image.
+  - **steps** (Integer, optional) - Number of generation steps. Higher is better but more expensive. Aim for 25-30.
+  - **sampler** (String, optional) - Sampler that will be used to denoise your image during generation. For advanced users.
+  - **scale** (Number, optional) - Determines how much the image generation process follows the text prompt. For advanced users
+
+#### Returns
+
+Returns a Promise that resolves to an Object with the following format:
+```js
+{
+  imageBatch: [ // array of generated images
+    base64: " /9j/4AAQSkZJRgABAQAAAQAQAQA...", // base64 encoded version of your generated image
+    base64: " /9j/4AAQSkZJRgABAQAAAQAABAB...", // base64 encoded version of your generated image  
+  ]
+  metadata: {
+    created: 1723815073228,
+    nanoCost: 0.00501,
+    remainingBalance: 0.13908
+  }
+}
+```
+
+### account()
+
+Retrieves information about your NanoGPT account.
+
+```js
+let account = await nanogpt.account()
+```
+
+Returns a Promise that resolves to an Object with the following format:
+```js
+{
+  nanoDepositAddress: 'nano_3d46mow3fim7bfd15xckfgao9ehabmp56y7gkn9q3nky1i3buehtroxi8q67',
+  nanoReturnAddress: 'nano_15qzsidyztg1i7at4zdeapzzh5hfgy1rip8e5uz1an3tujuunujgesgzf187',
+  balance: 3.098908,
+  receivable: 0,
+  earned: '0.000000'
+}
+```
+
+### balance()
+
+Retrieves your current NanoGPT account balance.
+
+```js
+let balance = await nanogpt.balance()
+```
+
+Returns a Number
+```js
+4.0678
+```
+
+### account()
+
+Retrieves information about the models available on NanoGPT.
+
+```js
+let models = await nanogpt.models()
+```
+
+Returns a Promise that resolves to an Object with the following format:
+```js
+{
+  "models": {
+    "text": {
+      "gpt-4o": {
+        "name": "GPT 4o",
+        "model": "gpt-4o",
+        "description": "OpenAI's most advanced model. It matches GPT-4 Turbo performance on text in English and code, with significant improvement on text in non-English languages, while also being much faster and 50% cheaper.",
+        "cost": "Average cost: 0.025 Nano",
+        "costEstimate": 0.025,
+        "labelVariant": "outline",
+        "label": "Recommended",
+        "visible": false
+      },
+      // ...more text models
+    },
+    "image": {
+      "dall-e-3": {
+        "name": "DALL-E-3",
+        "model": "dall-e-3",
+        "engine": "dalle",
+        "description": "The current golden standard in image generation.",
+        "cost": {
+          "1024x1024": 0.04,
+          "1024x1792": 0.08,
+          "1792x1024": 0.08
+        },
+        "label": "Default",
+        "maxImages": 1,
+        "resolutions": [
+          {
+            "value": "1024x1024"
+          },
+          {
+            "value": "1024x1792",
+            "comment": "2x the price"
+          },
+          {
+            "value": "1792x1024",
+            "comment": "2x the price"
+          }
+        ]
+      },
+      // ...more image models
+    }
+  }
+}
+```
+
+### contextUser(String content)
+
+Utility function to aid with creation of the Object field `context` in the `chat` function.
+
+```js
+nanogpt.contextUser('Blue is my favorite color')
+```
+
+#### Arguments
+
+- **content** (String, required) - Content that the user previously supplied via chat prompt.
+
+Returns an Object with the following format:
+```js
+{
+  role: 'user',
+  content: 'Blue is my favorite color'
+}
+```
+
+### contextAI(String content)
+
+Utility function to aid with creation of the Object field `context` in the `chat` function. 
+
+```js
+nanogpt.contextAI('Very interesting! I will remember that.')
+```
+
+#### Arguments
+
+- **content** (String, required) - Content that the AI previously replied with.
+
+Returns an Object with the following format:
+```js
+{
+  role: 'assistant',
+  content: 'Very interesting! I will remember that.'
+}
+```
+
+## Feedback
+
+Pull requests and opened issues are welcome!
+
+## Disclaimer
+
+As always when working with real world value, in this case Nano, be careful when using this library. The authors and contributors shall not be held liable for any use of this library's functionality, intentional or unintentional, that leads to an undesired lose of funds.
+
+## License
+
+MIT
---
+++ nanogptjs.js
@@ -0,0 +1,416 @@
+const NANOGPT_API_BASE_URL = 'https://nano-gpt.com/api/'
+const NANOGPT_API_TALK_TO_GPT = NANOGPT_API_BASE_URL + 'talk-to-gpt'
+const NANOGPT_API_TALK_TO_GEMINI = NANOGPT_API_BASE_URL + 'talk-to-gemini'
+const NANOGPT_API_GENERATE_IMAGE = NANOGPT_API_BASE_URL + 'generate-image'
+const NANOGPT_API_BALANCE = NANOGPT_API_BASE_URL + 'check-nano-balance'
+const NANOGPT_API_MODELS = NANOGPT_API_BASE_URL + 'models'
+
+const IMAGE_WIDTH = 1024
+const IMAGE_HEIGHT = 1024
+const IMAGE_NUM_STEPS = 25
+const IMAGE_SAMPLER_NAME = 'DPM++ 2S a Karras'
+const IMAGE_SCALE = 6.5
+
+const ERROR_PREFIX = '[NanoGPTJS error] '
+const ERROR_PROMPT_IS_NOT_A_STRING = 'Prompt must be a string'
+const ERROR_INCORRECT_PARAMETERS = 'Incorrect parameters'
+const ERROR_MODEL_NOT_SET = 'Model not set. Please specify model'
+const ERROR_NANOGPT_CONNECTION = 'Error during connection with NanoGPT, status: '
+
+module.exports = function ({
+  apiKey,
+  defaultModel
+}) {
+
+  const requestTemplate = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": apiKey,
+    }    
+  }
+
+  if (!apiKey) {
+    throw new Error(ERROR_PREFIX + 'NanoGPT API key not provided')
+  }
+
+  return {
+
+    chat: async function (params) {
+
+      let response
+      let prompt = ''
+      let model
+      let context = []
+
+      // if params are a single string then this is the prompt shorthand version of this function
+      if (isString(params)) {
+        prompt = params
+      } else if (isObject(params)) {
+        prompt = params.prompt
+        model = params.model
+        context = params.context
+      } else {
+        throw new Error(ERROR_PREFIX + ERROR_INCORRECT_PARAMETERS)
+      }
+
+      let _model = model || defaultModel
+
+      if (!_model) {
+        throw new Error(ERROR_PREFIX + ERROR_MODEL_NOT_SET)
+      }
+      if (!isString(prompt)) {
+        throw new Error(ERROR_PREFIX + ERROR_PROMPT_IS_NOT_A_STRING)
+      }
+      if (!isArray(context)) {
+        throw new Error(ERROR_PREFIX + 'Context must be an array')
+      }                    
+
+      let apiURL
+      if (_model.startsWith('gemini')) {
+        apiURL = NANOGPT_API_TALK_TO_GEMINI
+      } else {
+        apiURL = NANOGPT_API_TALK_TO_GPT
+      }
+
+      // -----
+
+      try {
+        response = await fetch(
+          apiURL,
+          Object.assign(requestTemplate,
+            {
+              body: JSON.stringify({
+                prompt,
+                model: _model,
+                messages: context
+              })
+            }
+          )
+        )
+
+        if (!response.ok) {
+          throw new Error()
+        }
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + ERROR_NANOGPT_CONNECTION + response.status)
+      }
+
+      // -----
+
+      try {
+        const response_ = await response.text()
+        const response__ = response_.split('<NanoGPT>')
+        const nanoGPTText = response__[0]
+        const responseMetadata = response__[1].split('</NanoGPT>')  
+        const responseMetadata_ = responseMetadata[0]
+        const nanoGPTMetadata = JSON.parse(responseMetadata_)
+    
+        return {
+          reply: nanoGPTText,
+          metadata: nanoGPTMetadata
+        }
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + error)
+      }      
+
+    },
+
+    // ---------------------------------------------------
+
+    image: async function (params) {
+
+      let response
+      let _params
+      
+      try {
+        _params = handleImageGenerationParams(params, defaultModel)
+      } catch (error) {
+        throw error
+      }
+
+      // -----
+
+      try {        
+        response = await fetch(
+          NANOGPT_API_GENERATE_IMAGE,
+          Object.assign(requestTemplate,
+            {
+              body: JSON.stringify({
+                prompt: _params.prompt,
+                model: _params.model,
+                width: _params.width,
+                height: _params.height,
+                negative_prompt: _params.negativePrompt,
+                num_steps: _params.steps,
+                resolution: _params.resolution,
+                sampler_name: _params.sampler,
+                scale: _params.scale,
+                nImages: 1
+              })
+            }
+          )
+        )    
+
+        if (!response.ok) {
+          throw new Error()
+        }
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + ERROR_NANOGPT_CONNECTION + response.status)
+      }        
+
+      // -----
+
+      try {
+        let parsedResponse = JSON.parse(await response.text())
+        let base64 = parsedResponse.data[0].b64_json
+        delete parsedResponse.data
+
+        return {
+          base64,
+          metadata: parsedResponse
+        }
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + error)
+      }      
+      
+    },
+
+    // ---------------------------------------------------
+
+    imageBatch: async function (params) {
+
+      let response
+      let _params
+      
+      if (!isObject(params)) {
+        throw new Error(ERROR_PREFIX + ERROR_INCORRECT_PARAMETERS)
+      }
+
+      try {
+        _params = handleImageGenerationParams(params, defaultModel)
+      } catch (error) {
+        throw error
+      }
+
+      // -----
+
+      try {
+    
+        response = await fetch(
+          NANOGPT_API_GENERATE_IMAGE,
+          Object.assign(requestTemplate,
+            {
+              body: JSON.stringify({
+                prompt: _params.prompt,
+                model: _params.model,
+                width: _params.width,
+                height: _params.height,
+                negative_prompt: _params.negativePrompt,
+                num_steps: _params.steps,
+                resolution: _params.resolution,
+                sampler_name: _params.sampler,
+                scale: _params.scale,
+                nImages: _params.batchSize
+              })              
+            }
+          )
+        )
+        
+        if (!response.ok) {
+          throw new Error()
+        }
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + ERROR_NANOGPT_CONNECTION + response.status)
+      }                
+
+      // -----
+
+      try {      
+        let parsedResponse = JSON.parse(await response.text())
+        let images_ = parsedResponse.data
+        let images = images_.map(function(i) {
+          return {base64: i.b64_json}
+        })
+        delete parsedResponse.data
+
+        return {
+          imageBatch: images,
+          metadata: parsedResponse
+        }
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + error)
+      }      
+      
+    },    
+
+    // ---------------------------------------------------
+
+    account: async function () {
+      let response
+
+      try {
+        response = await fetch( NANOGPT_API_BALANCE, requestTemplate )          
+        if (!response.ok) { throw new Error() }
+        return await response.json()
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + error)
+      }      
+      
+    },
+
+    // ---------------------------------------------------
+
+    balance: async function () {
+      let response
+
+      try {        
+        response = await fetch( NANOGPT_API_BALANCE, requestTemplate )          
+        if (!response.ok) { throw new Error() }      
+        return (await response.json()).balance
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + error)
+      }      
+      
+    },
+    
+
+    // ---------------------------------------------------
+
+    models: async function () {
+      let response
+
+      try {        
+        response = await fetch( NANOGPT_API_MODELS )          
+        if (!response.ok) { throw new Error() }      
+        return (await response.json())
+      }
+      catch (error) {
+        throw new Error(ERROR_PREFIX + error)
+      }      
+      
+    },    
+
+    // ---------------------------------------------------
+
+    contextUser: function(content = '') {  
+      return { role: 'user', content }
+    },
+
+    // ---------------------------------------------------
+
+    contextAI: function(content = '') {
+      return { role: 'assistant', content }     
+    }
+  }
+}
+
+// ---------------------------------------------------
+
+function handleImageGenerationParams(params, defaultModel) {
+  let _params = {}      
+
+  if (isString(params)) {
+    if (!defaultModel) {
+      throw new Error(ERROR_PREFIX + ERROR_MODEL_NOT_SET)
+    }
+
+    _params = {
+      prompt: params,
+      model: defaultModel,
+      width: IMAGE_WIDTH,
+      height: IMAGE_HEIGHT,
+      negativePrompt: '',
+      steps: IMAGE_NUM_STEPS,
+      sampler: IMAGE_SAMPLER_NAME,
+      scale: IMAGE_SCALE,
+      resolution: `${IMAGE_WIDTH}x${IMAGE_HEIGHT}`,
+      batchSize: 1
+    }
+    
+  }
+  else if (isObject(params)) {
+
+    _params = {
+      prompt: params.prompt || '',
+      model: params.model || defaultModel,
+      width: params.width || IMAGE_WIDTH,
+      height: params.height || IMAGE_HEIGHT,
+      negativePrompt: params.negativePrompt || '',
+      steps: params.steps || IMAGE_NUM_STEPS,
+      sampler: params.sampler || IMAGE_SAMPLER_NAME,
+      scale: params.scale || IMAGE_SCALE,
+      batchSize: params.batchSize || 1
+    }
+    _params.resolution = `${_params.width}x${_params.width}`
+
+  } else {
+    throw new Error(ERROR_PREFIX + ERROR_INCORRECT_PARAMETERS)
+  }
+
+  if (!isString(_params.prompt)) {
+    throw new Error(ERROR_PREFIX + ERROR_PROMPT_IS_NOT_A_STRING)
+  }      
+  if (!_params.model) {
+    throw new Error(ERROR_PREFIX + ERROR_MODEL_NOT_SET)
+  }
+  if (!isIntegerGreaterThanZero(_params.width)) {
+    throw new Error(ERROR_PREFIX + 'Image width must be an integer greater than zero')
+  }
+  if (!isIntegerGreaterThanZero(_params.height)) {
+    throw new Error(ERROR_PREFIX + 'Image height must be an integer greater than zero')
+  }  
+  if (!isString(_params.negativePrompt)) {
+    throw new Error(ERROR_PREFIX + 'Negative prompt must be a string')
+  }
+  if (!isIntegerGreaterThanZero(_params.steps)) {
+    throw new Error(ERROR_PREFIX + 'Number of steps must be an integer greater than zero')
+  }      
+  if (!isString(_params.sampler)) {
+    throw new Error(ERROR_PREFIX + 'Sampler name must be a string')
+  }
+  if (!isNumber(_params.scale)) {
+    throw new Error(ERROR_PREFIX + 'Scale must be a number')
+  }
+  if (!isIntegerGreaterThanZero(_params.batchSize)) {
+    throw new Error(ERROR_PREFIX + 'Number of images must be an integer greater than zero')
+  }      
+
+  return _params
+}
+
+// ---------------------------------------------------
+
+function isArray (value) {
+  return Object.prototype.toString.call(value) === '[object Array]'
+}
+
+function isString (value) {
+  return typeof value === 'string'
+}
+
+function isNumber (value) {
+  return typeof value === 'number'
+}
+
+function isInteger (value) {
+  return typeof value === 'number' &&
+    isFinite(value) &&
+    Math.floor(value) === value
+}
+
+function isIntegerGreaterThanZero (value) {
+  return isInteger(value) && value >= 1
+}
+
+function isObject (value) {
+  return value !== null && typeof value === 'object'
+}
